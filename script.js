5cedddb1b1a360bcb85a2a1a18bd43be613a87af
Arsh Ahmad <123231460+Ahmad-Arsh@users.noreply.github.com>
2023-04-19 22:39:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+// Get the line element
+var line = document.getElementById("line");
+
+// Set the initial rotation angle to 0
+var angle = 0;
+
+// Define a function to rotate the line
+function rotateLine() {
+  // Increment the rotation angle
+  angle += 1;
+
+  // Set the CSS transform to rotate the line
+  line.style.transform = "rotate(" + angle + "deg)";
+}
+
+// Call the rotateLine function every 10 milliseconds using setInterval
+setInterval(rotateLine, 10);
